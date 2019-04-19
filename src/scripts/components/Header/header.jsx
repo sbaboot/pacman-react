bb0c0a0e2ca701f29374dd16fd9b9a40b96aaac5
Sebastien Flamant <sebastienflamant31@gmail.com>
2019-04-19 07:05:36 +0200

    pacmanGouroux OK

--- src/scripts/components/Header/header.jsx
+++ src/scripts/components/Header/header.jsx
@@ -3,34 +3,34 @@ import ReactDOM from 'react-dom';
 
 class Header extends React.Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {points: 0, playing: true};
-	}
+  constructor(props) {
+    super(props);
+    this.state = { points: 0, playing: true };
+  }
 
-	componentDidMount() {
-		this.container = ReactDOM.findDOMNode(this);
-	}
+  componentDidMount() {
+    this.container = ReactDOM.findDOMNode(this);
+  }
 
-	increase() {
-		if (this.state.playing) {
-			var currentPoints = this.state.points + 1;
-			this.setState({points: currentPoints});
-		}
-	}
+  increase() {
+    if (this.state.playing) {
+      var currentPoints = this.state.points + 1;
+      this.setState({ points: currentPoints });
+    }
+  }
 
-	gameOver() {
-		this.setState({playing:false, points: 'GAME OVER'});
-	}
+  gameOver() {
+    this.setState({ playing: false, points: 'GAME OVER' });
+  }
 
-	render() {
-		return (
-			<div className="header">
-				<span className="left title">PACMAN</span>
-				<span className="right score">SCORE: <span className="points">{this.state.points}</span></span>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="header">
+        <span className="left title">Aide le christ cosmique à voler les poules request</span>
+        <span className="right score">Score cosmique: <span className="points">{this.state.points}</span></span>
+      </div>
+    );
+  }
 }
 
 Header.defaultProps = {
